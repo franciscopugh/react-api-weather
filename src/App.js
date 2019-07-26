6ee4ecd62c25d60dfe0e2a887816a82d6591e849
TheDarkWolf <franciscopugh01@gmail.com>
2019-07-25 23:45:06 -0300

    Form component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Titles from "./components/Titles";
+import Form from "./components/Form";
+
 class App extends React.Component{
     render() {
         return (
             <div>
                 <Titles/>
+                <Form/>
             </div>
         );
     }
---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Form extends React.Component {
+    render(){
+        
+        return(
+            <div>
+                <h3>Form Component</h3>
+            </div>
+        );
+    }
+}
+
+export default Form;
