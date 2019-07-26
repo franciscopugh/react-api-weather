71ea752fcc7fbeb746eada9dfe0df4654dea3169
TheDarkWolf <franciscopugh01@gmail.com>
2019-07-25 22:49:59 -0300

    First Component: Titles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Titles from "./components/Titles";
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                <Titles/>
+            </div>
+        );
+    }
+};
+
+export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component{
+    
+    render() {
+        return(
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Find Out Temperature, Conditions and more...</p>
+            </div>
+        );
+    }
+
+};
+
+export default Titles;
