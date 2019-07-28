cd6d9806bebd5c56eb337484438cca33dcaca6d0
TheDarkWolf <franciscopugh01@gmail.com>
2019-07-28 16:06:43 -0300

    Api data received

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "bfd1b980aa5416c251b43fb2f1ba6c22";
+
 class App extends React.Component{
+    getWeather = async(e) => {
+        e.preventDefault();
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+        const data = await  api_call.json();
+        console.log(data);
+    }
     render() {
         return (
             <div>
                 <Titles/>
-                <Form/>
+                <Form getWeather={this.getWeather}/>
                 <Weather/>
             </div>
         );
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,11 @@ class Form extends React.Component {
     render(){
         
         return(
-            <div>
-                <h3>Form Component</h3>
-            </div>
+            <form onSubmit={this.props.getWeather}>
+                <input type="text" name="city" placeholder="City"/>
+                <input type="text" name="country" placeholder="Country"/>   
+                <button>Get Weather</button>
+            </form>
         );
     }
 }
