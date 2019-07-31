f523823128b202f1019622fdb95dbe38fc34085d
TheDarkWolf <franciscopugh01@gmail.com>
2019-07-30 22:48:11 -0300

    Graphical interface start

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,9 +2,8 @@ import React from "react";
 
 const Titles = () => (
             <div>
-                <h1>Weather Finder</h1>
-                <p>Find Out Temperature, Conditions and more...</p>
+                <h1 className="title-container__title">Weather Finder</h1>
+                <p className="title-container__subtitle">Find Out Temperature, Conditions and more...</p>
             </div>
 );
-
 export default Titles;
