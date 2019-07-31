0329629ce6266364b9a92c1c026b2d05734e7582
TheDarkWolf <franciscopugh01@gmail.com>
2019-07-31 00:08:12 -0300

    Graphical interface terminated

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Titles = () => (
             <div>
-                <h1 className="title-container__title">Weather Finder</h1>
-                <p className="title-container__subtitle">Find Out Temperature, Conditions and more...</p>
+                <h1 className="title-container__title">Weather App</h1>
+                <p className="title-container__subtitle">Find Out Temperature, Humidity and more...</p>
             </div>
 );
 export default Titles;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,27 @@
 import React from "react";
 
  const Weather = props => (
-        <div>
-                { props.city && props.country && <p>Location:{ props.city }, { props.country }</p>}
-                { props.temperature && <p>Temperature: { props.temperature }</p> }
-                { props.humidity && <p>Humidity: { props.humidity + '%'}</p> }
-                { props.description && <p>Condicions: { props.description }</p>} 
-                { props.error && <p>{ props.error }</p>}
+        <div className="weather__info">
+                { 
+                props.city && props.country && <p className="weather__key">Location:
+                <span className="weather__value">{ props.city }, { props.country }</span></p>
+                }
+                { 
+                props.temperature && <p className="weather__key">Temperature: 
+                <span className="weather__value">{ props.temperature + '℃' }</span></p> 
+                }
+                { 
+                props.humidity && <p className="weather__key">Humidity: 
+                <span className="weather__value">{ props.humidity + '%'}</span></p> 
+                }
+                {
+                props.description && <p className="weather__key">Condicions: 
+                <span className="weather__value">{ props.description }</span></p>
+                } 
+                {
+                props.error && <p className="weather__error">
+                { props.error }</p>
+                }
         </div>
 );
 export default Weather;
